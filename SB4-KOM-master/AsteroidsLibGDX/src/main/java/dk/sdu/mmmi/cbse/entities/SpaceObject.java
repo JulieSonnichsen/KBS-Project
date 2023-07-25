cc8fa5d8b730998c414d0d33a57387d6e2dabca9
JulieSonnichsen <93269568+JulieSonnichsen@users.noreply.github.com>
2023-07-25 12:10:06 +0200

    Lab 1

--- SB4-KOM-master/AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/entities/SpaceObject.java
+++ SB4-KOM-master/AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/entities/SpaceObject.java
@@ -2,32 +2,47 @@ package dk.sdu.mmmi.cbse.entities;
 
 import dk.sdu.mmmi.cbse.main.Game;
 
-public class SpaceObject {
-	
+abstract public class SpaceObject {
+
 	protected float x;
 	protected float y;
-	
+
 	protected float dx;
 	protected float dy;
-	
+
 	protected float radians;
 	protected float speed;
 	protected float rotationSpeed;
-	
+
 	protected int width;
 	protected int height;
-	
+
 	protected float[] shapex;
 	protected float[] shapey;
-	
+
+	protected float[] color;
+
 	protected void wrap() {
 		if(x < 0) x = Game.WIDTH;
 		if(x > Game.WIDTH) x = 0;
 		if(y < 0) y = Game.HEIGHT;
-		if(y > Game.HEIGHT) y = 0; 
+		if(y > Game.HEIGHT) y = 0;
 	}
-	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- SB4-KOM-master/AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/managers/GameInputProcessor.java
+++ SB4-KOM-master/AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/managers/GameInputProcessor.java
@@ -4,18 +4,18 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Input.Keys;
 
 public class GameInputProcessor extends InputAdapter {
-	
+
 	public boolean keyDown(int k) {
-		if(k == Keys.UP) {
+		if(k == Keys.UP || k == Keys.W) {
 			GameKeys.setKey(GameKeys.UP, true);
 		}
-		if(k == Keys.LEFT) {
+		if(k == Keys.LEFT || k == Keys.A) {
 			GameKeys.setKey(GameKeys.LEFT, true);
 		}
-		if(k == Keys.DOWN) {
+		if(k == Keys.DOWN || k == Keys.S) {
 			GameKeys.setKey(GameKeys.DOWN, true);
 		}
-		if(k == Keys.RIGHT) {
+		if(k == Keys.RIGHT || k == Keys.D) {
 			GameKeys.setKey(GameKeys.RIGHT, true);
 		}
 		if(k == Keys.ENTER) {
@@ -32,18 +32,18 @@ public class GameInputProcessor extends InputAdapter {
 		}
 		return true;
 	}
-	
+
 	public boolean keyUp(int k) {
-		if(k == Keys.UP) {
+		if(k == Keys.UP|| k == Keys.W) {
 			GameKeys.setKey(GameKeys.UP, false);
 		}
-		if(k == Keys.LEFT) {
+		if(k == Keys.LEFT || k == Keys.A) {
 			GameKeys.setKey(GameKeys.LEFT, false);
 		}
-		if(k == Keys.DOWN) {
+		if(k == Keys.DOWN || k == Keys.S) {
 			GameKeys.setKey(GameKeys.DOWN, false);
 		}
-		if(k == Keys.RIGHT) {
+		if(k == Keys.RIGHT || k == Keys.D) {
 			GameKeys.setKey(GameKeys.RIGHT, false);
 		}
 		if(k == Keys.ENTER) {
@@ -60,8 +60,10 @@ public class GameInputProcessor extends InputAdapter {
 		}
 		return true;
 	}
-	
+
 }
+
+
 
 
 
